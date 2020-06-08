e9db6d14211901fd54c62dc9571d1ee439f076ec
sergio audel ortiz gutierrez <audel91@gmail.com>
2020-06-07 23:23:49 -0700

    removing unused dependencies

--- src/main/kotlin/com/saudel/ranchoapi/RanchoapiApplication.kt
+++ src/main/kotlin/com/saudel/ranchoapi/RanchoapiApplication.kt
@@ -1,9 +1,7 @@
 package com.saudel.ranchoapi
 
-import com.expediagroup.graphql.spring.operations.Query
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.stereotype.Component
 
 @SpringBootApplication
 class RanchoapiApplication
